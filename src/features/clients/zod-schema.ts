d08d0ef2a20b20a5e48cda7257eb33d85008dfb0
byakuDNY <r13luo@gmail.com>
2025-03-05 15:50:39 -0500

    update clientInsertSchema to enforce minimum length for name, contact, and phone fields

--- src/features/clients/zod-schema.ts
+++ src/features/clients/zod-schema.ts
@@ -1,9 +1,11 @@
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { z } from "zod";
 
 import { clients } from "@/db/schema";
 
 export const clientInsertSchema = createInsertSchema(clients, {
-  email: z.string().email(),
+  name: (schema) => schema.min(2),
+  email: (schema) => schema.email(),
+  contact: (schema) => schema.min(2),
+  phone: (schema) => schema.min(6),
 });
 export const clientSelectSchema = createSelectSchema(clients);
